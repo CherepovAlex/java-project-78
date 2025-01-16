7045b2eac36ec0932e856671192ef9485a9d7dd9
CherepovAlex <westler123@yandex.ru>
2025-01-16 23:57:58 +0300

    check App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,9 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
-
-import java.util.HashMap;
-import java.util.Map;
+//import hexlet.code.schemas.BaseSchema;
+//
+//import java.util.HashMap;
+//import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
