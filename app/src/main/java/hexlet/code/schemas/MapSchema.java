ddb299bfbbc52465c506628edef5502df867d852
CherepovAlex <westler123@yandex.ru>
2025-01-17 11:01:14 +0300

    fix tests

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,7 +2,7 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public class MapSchema extends BaseSchema<Map<String, Object>> {
+public class MapSchema extends BaseSchema<Map<String, ?>> {
 
     public MapSchema required() {
         addValidation("required", o -> o != null);
@@ -10,26 +10,25 @@ public class MapSchema extends BaseSchema<Map<String, Object>> {
     }
 
     public MapSchema sizeof(int size) {
-        addValidation("minLength", o -> o.size() == size);
+        addValidation("sizeof", o -> o.size() == size);
         return this;
     }
 
     public <T> MapSchema shape(Map<String, BaseSchema<T>> schemas) {
-        addValidation("shape",
-                map -> {
-                    return schemas.entrySet().stream().allMatch(entry -> {
-                        String key = entry.getKey();
-                        BaseSchema<T> schema = entry.getValue();
-                        if (!map.containsKey(key)) {
-                            return false;
-                        }
-                        T value = (T) map.get(key);
-                        if (value == null) {
-                            return false;
-                        }
-                        return schema.isValid(value);
-                    });
+        addValidation("shape", map -> {
+            return schemas.entrySet().stream().allMatch(entry -> {
+                String key = entry.getKey();
+                BaseSchema<T> schema = entry.getValue();
+                if (!map.containsKey(key)) {
+                    return false;
                 }
+                T value = (T) map.get(key);
+                if (value == null) {
+                    return false;
+                }
+                return schema.isValid(value);
+            });
+            }
         );
         return this;
     }
